c2d6d6987adf764a9151311a9c92555945166a69
MalogPeter <2557826346@qq.com>
2023-07-22 12:01:18 +0800

    l

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Abobo';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Abobo的个人博客，记录生活，分享技术，时常发癫';
+    : '心事浩茫连广宇，于无声处听惊雷。';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
