f314745e1c02cc96764cbf6c65655704ec59270c
Abobo8964 <2557826346@qq.com>
2023-12-04 14:54:22 +0800

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,6 @@ import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
 
 export default function Index({ posts, globalData }) {
-  // 将您的音乐链接放在这里
-  const musicUrl = "https://music.163.com/song/media/outer/url?id=28263184";
 
   return (
     <Layout>
@@ -50,12 +48,6 @@ export default function Index({ posts, globalData }) {
         </ul>
       </main>
       <Footer copyrightText={globalData.footerText} />
-      <div className="audio-player">
-        <audio controls>
-          <source src={musicUrl} type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
-      </div>
       <GradientBackground
         variant="large"
         className="fixed top-20 opacity-40 dark:opacity-60"
